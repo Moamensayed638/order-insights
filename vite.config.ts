55be2c6674c3379f4726be8e98c78b2ff17258a1
ahmedgamalalzatary <ahmedalzatary716@gmail.com>
2026-04-29 23:46:39 +0200

    Update API base URL and remove proxy configuration in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,6 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    proxy: {
-      "/api": {
-        target: "https://biscofa.runasp.net",
-        changeOrigin: true,
-        secure: true,
-      },
-    },
     hmr: {
       overlay: false,
     },
