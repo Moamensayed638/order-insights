a76a7ad85f9bb61179fc0c5041e0fc8aa23a27e4
ahmedgamalalzatary <ahmedalzatary716@gmail.com>
2026-04-30 00:31:26 +0200

    fix amdinorders redirects page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -12,6 +12,11 @@ import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
 
+function RequireAuth({ children }: { children: JSX.Element }) {
+  useLocation();
+  return getStoredToken() ? children : <Login />;
+}
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
@@ -19,11 +24,11 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={getStoredToken() ? <Index /> : <Login />} />
+          <Route path="/" element={<RequireAuth><Index /></RequireAuth>} />
           <Route path="/login" element={<Login />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/adminorders" element={getStoredToken() ? <Index /> : <Login />} />
+          <Route path="/adminorders" element={<RequireAuth><Index /></RequireAuth>} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
